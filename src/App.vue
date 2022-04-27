e11520827be10768f9987f10ff2acbffe7d4ab24
Cintia Arce <cintiaayelenarce@gmail.com>
2022-04-27 18:37:24 -0300

    template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav>
+      <h1>Player</h1>
+    </nav>
+    <main>
+      <div class="player">
+        <div class="controller">
+          <button>Back</button>
+          <button>Play</button>
+          <button>Next</button>
+        </div>
+        <div class="current">
+          <h3 class="current-song">
+            {{ current.songTitle }}
+          </h3>
+          <h4 class="current artist">
+            {{ current.artist }}
+          </h4>
+        </div>
+        <div class="music-list">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      current: {
+        songTitle: 'Song Title',
+        artist: 'Artist Name'
+      },
+      songs: [
+        {
+          songTitle: 'Boogie Wonderland',
+          artist: 'Earth, Wind & Fire',
+          src: ''
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
