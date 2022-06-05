01f1987e257e09b5a6df666aede154019abf7292
Cintia Arce <cintiaayelenarce@gmail.com>
2022-06-05 15:41:53 -0300

    responsive header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="navbar bg-yellow-200 p-2">
-        <div class=" navbar-container p-5 flex flex-row justify-evenly items-center">
-            <div class="title-container">
-                <h1 class="navbar-title font-bold tracking-wider text-xl text-white">Loofi Player</h1>
+    <header class="relative w-full flex flex-wrap items-center justify-between py-3 bg-yellow-200 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+        <nav class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+            <div class="container-fluid">
+                <h1 class="font-bold tracking-wider text-xl text-white">Loofi Player</h1>
             </div>
-        </div>
-  </nav>
+        </nav>
+  </header>
 </template>
 
 <script>
