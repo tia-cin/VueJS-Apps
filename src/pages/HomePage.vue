8b6736ba59c2b9573b81eaad8e80089b83120882
cintia arce <cintiaayelenarce@gmail.com>
2022-11-29 18:56:53 -0300

    navbar fixed

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="mt-20 flex justify-evenly items-center">
-    <nav class="bg-slate-300 fixed min-w-full flex items-center p-3">
+  <div>
+    <nav class="bg-slate-300 top-0 fixed min-w-full flex items-center p-3">
       <img :src="logo" class="w-6 h-6" />
-      <span class="font-semibold">Projects</span>
+      <span class="font-semibold text-2xl">Projects</span>
     </nav>
-    <header class="mb-20">
-      <h1 class="text-6xl font-semibold">Explore Vue JS projects</h1>
-      <p class="text-2xl mt-5">Witch one would you like to go first?</p>
-    </header>
-    <section class="mt-5">
-      <router-link to="/music">
-        <button
-          class="flex items-center bg-emerald-500 rounded-xl p-2 cursor-pointer"
-        >
-          <img :src="music" class="mx-2 w-10 h-10" />
-          <span class="text-xl text-white font-medium mx-2">
-            Music Player
-          </span>
-        </button>
-      </router-link>
-    </section>
+    <main class="flex flex-col items-center mt-20">
+      <header class="mb-20">
+        <h1 class="text-6xl font-semibold">Explore Vue JS projects</h1>
+        <p class="text-2xl mt-5">Witch one would you like to go first?</p>
+      </header>
+      <section class="mt-5">
+        <router-link to="/music">
+          <button
+            class="flex items-center bg-emerald-500 rounded-xl p-2 cursor-pointer"
+          >
+            <img :src="music" class="mx-2 w-10 h-10" />
+            <span class="text-xl text-white font-medium mx-2">
+              Music Player
+            </span>
+          </button>
+        </router-link>
+      </section>
+    </main>
   </div>
 </template>
 
