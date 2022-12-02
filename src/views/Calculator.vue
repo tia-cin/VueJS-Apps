9b8e239793612989a4750f2c484ca372c784580d
cintia arce <cintiaayelenarce@gmail.com>
2022-12-02 16:04:41 -0300

    fix style

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex flex-col justify-center items-center">
         <h2 class="text-4xl font-semibold my-5">Calculator</h2>
-        <div class="p-5 bg-slate-400 text-5xl rounded">0</div>
-        <section class="grid grid-cols-4 gap-0">
+            <div class="border-black border p-5 bg-slate-400 text-5xl rounded w-260 text-white text-right">0</div>
+            <section class="grid grid-cols-4 gap-0">
             <div 
                 v-for="button in buttons" 
                 :key="button.value" 
-                class="border-black border rounded p-5" 
+                class="border-black border rounded p-5 cursor-pointer" 
                 :class="((button.id === 'gray' && 'bg-gray-400') || (button.id === 'orange' && 'bg-orange-300'))"
-            >
+                >
                 <span class="text-center">{{ button.value }}</span>
             </div>
         </section>
