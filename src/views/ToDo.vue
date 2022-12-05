3298cc10e811db15efcffb7794a22efe7a27ae3f
cintia arce <cintiaayelenarce@gmail.com>
2022-12-05 15:51:12 -0300

    add methods to template

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -2,23 +2,42 @@
     <div class="flex flex-col items-center">
         <ProjectTitle title="To Do List"/>
         <div class="flex justify-around">
-            <form class="flex justify-between">
+            <form class="flex justify-between" @submit.prevent="addTodo">
                 <input 
+                    v-model="newTodo"
                     class="bg-slate-200 rounded-xl p-2" 
-                    placeholder="New To To" 
-                    name="newTodo" 
+                    placeholder="New To Do" 
                     type="text" 
                     value=""
                 /> 
-                <button class="bg-yellow-400 rounded-xl p-2 mx-2 font-medium" type="submit">Add</button>
+                <button 
+                    class="bg-yellow-400 rounded-xl p-2 mx-2 font-medium" 
+                    type="submit"
+                >
+                    Add
+                </button>
             </form>
-            <button class="bg-blue-200 rounded-xl p-2 mx-5 font-medium">Complete All</button>
+            <button 
+                @click="completeAll" 
+                class="bg-blue-200 rounded-xl p-2 mx-5 font-medium"
+            >
+                Complete All
+            </button>
         </div>
         <ul class="grid grid-cols-4 gap-2 mt-10">
-            <li class="flex justify-between items-center w-300">
-                <input type="checkbox"/>
-                <p class="capitalize font-semibold text-xl">todo</p>
-                <button class="bg-red-500 p-2 rounded-xl font-medium">Remove</button>
+            <li 
+                class="flex justify-between items-center w-300" 
+                v-for="todo in todos" 
+                :key="todo.title"
+            >
+                <input type="checkbox" v-model="todo.done"/>
+                <p class="capitalize font-semibold text-xl">{{todo.title}}</p>
+                <button 
+                    class="bg-red-500 p-2 rounded-xl font-medium" 
+                    @click="removeTodo(todo.title)"
+                >
+                    Remove
+                </button>
             </li>
         </ul>
     </div>
