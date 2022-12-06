e7201b60931874efe2c4855f8294a9597c091a4e
cintia arce <cintiaayelenarce@gmail.com>
2022-12-06 10:45:23 -0300

    template

--- src/views/WeatherApp.vue
+++ src/views/WeatherApp.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <ProjectTitle title="Weather App"/>
+        <form>
+            <input type="text" placeholder="Search"/>
+            <button>🔍</button>
+        </form>
+        <div>
+            <img/>
+            <header>
+                <p></p>
+                <span></span>
+            </header>
+            <article>
+                <p>°c</p>
+                <span></span>
+            </article>
+        </div>
     </div>
 </template>
 
@@ -9,6 +24,12 @@ import ProjectTitle from '../components/Title.vue';
 
 export default {
     name: "WeatherApp",
-    components: { ProjectTitle }
+    components: { ProjectTitle },
+    data: () => ({
+      api_key: process.env.API_KEY,
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      search: '',
+      weather: {}
+    }),
 }
 </script>
