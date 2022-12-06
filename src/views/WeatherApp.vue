6d35fa1463117d0d5822ca9ba77d1d7d48a95e76
cintia arce <cintiaayelenarce@gmail.com>
2022-12-06 11:24:15 -0300

    card

--- src/views/WeatherApp.vue
+++ src/views/WeatherApp.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
+    <div class="flex flex-col items-center">
         <ProjectTitle title="Weather App"/>
         <form @submit.prevent="fetchWeather">
-            <input type="text" placeholder="Search" v-model="search"/>
-            <button type="submit">🔍</button>
+            <input type="text" placeholder="Search" v-model="search" class="bg-slate-200 rounded-lg p-2 mx-2"/>
+            <button type="submit" class="bg-orange-400 p-2 texl-xl rounded-xl">🔍</button>
         </form>
-        <div v-if="weather.main">
-            <img alt="weather-type" :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`"/>
-            <header>
-                <p>{{ weather.name }}, {{ weather.sys.country }}</p>
-                <span>{{ dateBuilder() }}</span>
+        <div v-if="weather.main" class="drop-shadow-xl bg-slate-200 rounded-xl w-300 mt-10 flex flex-col items-center">
+            <img 
+                alt="weather-type" 
+                :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`"
+                class="w-100"
+            />
+            <header class="text-center flex items-center">
+                <p class="text-2xl font-semibold">{{ weather.name }}, {{ weather.sys.country }}</p>
+                <span class="text-sm mt-1.5 mx-2">{{ dateBuilder() }}</span>
             </header>
             <article>
-                <p>{{ Math.round(weather.main.temp) }}°c</p>
+                <p>{{ Math.round(weather.main.temp) }}°C</p>
                 <span>{{ weather.weather[0].main }}</span>
             </article>
         </div>
@@ -45,7 +49,7 @@ export default {
         },
         dateBuilder () {
             const d = new Date();
-            return `${d.getDay()} ${d.getDate()} ${d.getMonth()} ${d.getFullYear()}`;
+            return `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
         }
     },
     beforeMount() {
